f5fa53dc168569768968b0294baec2478c5ffb78
BarnYard <barnyard22993@gmail.com>
2025-08-13 13:26:30 +0300

    implemented color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/ColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/ColorSensor.java
@@ -10,34 +10,71 @@ public class ColorSensor {
 
     private final NormalizedColorSensor colorSensor;
     private final DistanceSensor distanceSensor;
+    private final double SAMPLE_DETECTION_RANGE = 7;
+    private final double RED_TO_GREEN_YELLOW_RATIO = 1.5;
+
+    private final double DATA_TRACKING_LOOPS_DELAY = 10;
+    private double loop_tracker;
+
+    private boolean cashedIsSampleDetected;
+    private SampleColor cashedSampleColor;
 
     public ColorSensor(NormalizedColorSensor colorSensor) {
         this.colorSensor = colorSensor;
         this.distanceSensor = (DistanceSensor) colorSensor;
         this.colorSensor.setGain(GAIN);
+
+        cashedSampleColor = SampleColor.UNKNOWN;
+        cashedIsSampleDetected = false;
+
+        loop_tracker = 0;
     }
 
-    public double getDistance() {
+    private double getDistance() {
         return distanceSensor.getDistance(DistanceUnit.CM);
     }
 
-    public float getRed() {
+    private float getRed() {
         return colorSensor.getNormalizedColors().red;
     }
 
-    public float getGreen() {
+    private float getGreen() {
         return colorSensor.getNormalizedColors().green;
     }
 
-    public float getBlue() {
+    private float getBlue() {
         return colorSensor.getNormalizedColors().blue;
     }
 
 
-    public boolean isSampleDetected() {
-        return false;
+    private boolean determineIsSampleDetected() {
+        return getDistance() <= SAMPLE_DETECTION_RANGE;
     }
+    private SampleColor determineSampleColor() {
+        if (getBlue() > getRed() && getBlue() > getGreen()) {
+            return SampleColor.BLUE;
+        }
+        else if (getRed() / getGreen() > RED_TO_GREEN_YELLOW_RATIO) {
+            return SampleColor.RED;
+        }
+        else {
+            return SampleColor.YELLOW;
+        }
+    }
+    public void operate() {
+        if (loop_tracker >= DATA_TRACKING_LOOPS_DELAY) {
+            determineSampleColor();
+            determineIsSampleDetected();
+            loop_tracker = 0;
+        }
+        loop_tracker++;
+    }
+
+    public boolean isSampleDetected(){
+        return cashedIsSampleDetected;
+    }
+
     public SampleColor getSampleColor() {
-        return SampleColor.UNKNOWN;
+        return cashedSampleColor;
     }
 }
